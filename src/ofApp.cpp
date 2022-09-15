4fb43fd92e2411eee41dddd26c163204317570af
tillmanjex <tillmanjex@mailbox.org>
2022-09-15 21:45:54 +0200

    deleted edges / polyline code

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -94,15 +94,6 @@ void ofApp::draw(){
         circle->draw();
     };
     
-
-//    for (auto &edge : tEdges){
-//        ofSetColor(255, 0, 80);
-//        ofPushMatrix();
-//        ofTranslate(canvasCenter.x, canvasCenter.y);
-//        edge->setPosition(canvasCenter);
-//        edge->draw();
-//        ofPopMatrix();
-//    };
     
     
     for (auto &rect : tRects){
@@ -175,67 +166,7 @@ void ofApp::onSliderEvent(ofxDatGuiSliderEvent e){
     } else if (e.target->is("Tombola Rotate")){
         tombolaRotate(e.value);
     }
-    
-//    // clear polylines for fresh input
-//    tLine0.clear();
-//    tLine1.clear();
-//    tLine2.clear();
-//    tLine3.clear();
-//    tLine4.clear();
-//    tLine5.clear();
-//
-//    for (auto &edge : tEdges){
-//        // clear &edge on each iteration for correct drawing behavior
-//        edge->clear();
-//
-//        if (e.target->is("Tombola Size")){
-//
-//            v0.set(e.value * cos(glm::radians(0.0)), e.value * sin(glm::radians(0.0)), 0);
-//            v1.set(e.value * cos(glm::radians(60.0)), e.value * sin(glm::radians(60.0)), 0);
-//            v2.set(e.value * cos(glm::radians(120.0)), e.value * sin(glm::radians(120.0)), 0);
-//            v3.set(e.value * cos(glm::radians(180.0)), e.value * sin(glm::radians(180.0)), 0);
-//            v4.set(e.value * cos(glm::radians(240.0)), e.value * sin(glm::radians(240.0)), 0);
-//            v5.set(e.value * cos(glm::radians(300.0)), e.value * sin(glm::radians(300.0)), 0);
-//
-//        } else if (e.target->is("Tombola Rotate")) {
-//
-//            v0.rotate(e.value, ofVec3f(0, 0, 1));
-//            v1.rotate(e.value, ofVec3f(0, 0, 1));
-//            v2.rotate(e.value, ofVec3f(0, 0, 1));
-//            v3.rotate(e.value, ofVec3f(0, 0, 1));
-//            v4.rotate(e.value, ofVec3f(0, 0, 1));
-//            v5.rotate(e.value, ofVec3f(0, 0, 1));
-//        }
-//            // add the updated verticies to the polyline objects
-//            tLine0.addVertex(v0);
-//            tLine0.addVertex(v1);
-//
-//            tLine1.addVertex(v1);
-//            tLine1.addVertex(v2);
-//
-//            tLine2.addVertex(v2);
-//            tLine2.addVertex(v3);
-//
-//            tLine3.addVertex(v3);
-//            tLine3.addVertex(v4);
-//
-//            tLine4.addVertex(v4);
-//            tLine4.addVertex(v5);
-//
-//            tLine5.addVertex(v5);
-//            tLine5.addVertex(v0);
-//
-//            // add the updated polylines to edges
-//            edge->addVertexes(tLine0);
-//            edge->addVertexes(tLine1);
-//            edge->addVertexes(tLine2);
-//            edge->addVertexes(tLine3);
-//            edge->addVertexes(tLine4);
-//            edge->addVertexes(tLine5);
-//            edge->create(box2d.getWorld());
-//
-//    };
-    
+ 
     
 }
 
@@ -310,28 +241,6 @@ void ofApp::tombolaInit(){
     v3r.set(canvasCenter.x + v3.x - (v4.x / 2), canvasCenter.y + v3.y - (v4.y / 2));
     v4r.set(canvasCenter.x + v4.x - (v5.x / 2), canvasCenter.y + v4.y - (v5.y / 2));
     v5r.set(canvasCenter.x + v5.x - (v0.x / 2), canvasCenter.y + v5.y - (v0.y / 2));
-    
-    
-    // Creating polylines for use in ofxBoxEdges
-    // Could probably also do this in a vector, but got confused by how a vector of polylines works
-    // (using the ofVec3s here instead of calculating inside incase I need easy access to them as variables...)
-    tLine0.addVertex(v0);
-    tLine0.addVertex(v1);
-    
-    tLine1.addVertex(v1);
-    tLine1.addVertex(v2);
-    
-    tLine2.addVertex(v2);
-    tLine2.addVertex(v3);
-    
-    tLine3.addVertex(v3);
-    tLine3.addVertex(v4);
-    
-    tLine4.addVertex(v4);
-    tLine4.addVertex(v5);
-    
-    tLine5.addVertex(v5);
-    tLine5.addVertex(v0);
 
     // this feels stupid, not sure if it's the best way.
     // But as each rect needs a differnt initial position and rotation value
@@ -365,18 +274,6 @@ void ofApp::tombolaInit(){
     tRects.push_back(rect4);
     tRects.push_back(rect5);
 
-    
-    // Creating edges from polylines
-    auto edge = std::make_shared<ofxBox2dEdge>();
-    edge->addVertexes(tLine0);
-    edge->addVertexes(tLine1);
-    edge->addVertexes(tLine2);
-    edge->addVertexes(tLine3);
-    edge->addVertexes(tLine4);
-    edge->addVertexes(tLine5);
-    edge->create(box2d.getWorld());
-    tEdges.push_back(edge);
-    
     
 }
 
